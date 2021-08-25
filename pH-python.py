10b59078a9f149cabb70ad2a1487992250ade72d
j0n4as0sv4ald0 <78802852+jonasosvaldopinto@users.noreply.github.com>
2021-08-25 13:39:29 -0300

    Add files via upload

---
+++ pH-python.py
@@ -0,0 +1,13 @@
+
+Valor_do_pH = int(input("o valor do pH"))
+
+if Valor_do_pH == 7:
+    print("o pH é neutro")
+
+if Valor_do_pH < 7:
+    print("o valor do pH é acido")
+    
+elif Valor_do_pH > 7:
+    print("o valor do pH é basico")
+    
+
